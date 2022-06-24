48e35fff85d4adfd20b86ebb1ad4cab57a2c1665
piedpipr <43669876+piedpipr@users.noreply.github.com>
2022-06-24 23:05:06 +0600

    Springboot backend project init

---
+++ backend/src/main/java/me/protik/simplyblog/SimplyblogApplication.java
@@ -0,0 +1,13 @@
+package me.protik.simplyblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplyblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplyblogApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/me/protik/simplyblog/SimplyblogApplicationTests.java
@@ -0,0 +1,13 @@
+package me.protik.simplyblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplyblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
