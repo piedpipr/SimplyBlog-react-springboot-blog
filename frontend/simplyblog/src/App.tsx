3226275074f9a120197cfde9527f4746e25134d5
piedpipr <43669876+piedpipr@users.noreply.github.com>
2022-07-09 01:56:51 +0600

    simple navbar

--- frontend/simplyblog/src/App.tsx
+++ frontend/simplyblog/src/App.tsx
@@ -3,10 +3,12 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import Home from './pages/home';  
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/navbar';
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <Home />
     </Router>
   );
