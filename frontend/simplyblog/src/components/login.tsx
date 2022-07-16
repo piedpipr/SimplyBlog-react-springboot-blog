196f981793ecf5a3adb2cbd1d1da540767b07875
piedpipr <43669876+piedpipr@users.noreply.github.com>
2022-07-16 19:16:05 +0600

    basic login and profile page with localstorage jwt

--- frontend/simplyblog/src/components/login.tsx
+++ frontend/simplyblog/src/components/login.tsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import { useState } from 'react';
+import axios from '../api/axios';
 
+const LOGIN_API = '/authenticate';
 type Props = {}
 
 export default function Login({}: Props) {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async (e:any) => {
+    e.preventDefault();
+    const response = await axios.post(LOGIN_API,
+      {
+        userName: username,
+        password: password,
+      }
+      );
+    console.log(response.data.jwt);
+    localStorage.setItem('token', response.data.jwt);
+    
+
+  }
   return (
-    <div>login</div>
+    <div onSubmit={handleSubmit}>
+      <form>
+        <label htmlFor="username">Username</label>
+        <input
+        type="text"
+        id="username"
+        onChange={(e) => setUsername(e.target.value)}
+        required/>
+        <label htmlFor="password">Password</label>
+        <input
+        type="password"
+        id="password"
+        onChange={(e) => setPassword(e.target.value)}
+        required/>
+        <button type="submit">Login</button>
+      </form>
+    </div>
   )
 }
--- frontend/simplyblog/src/components/profile.tsx
+++ frontend/simplyblog/src/components/profile.tsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import { useState, useEffect } from 'react';
+import axios from '../api/axios';
+
+const MY_PROFILE_API = '/user/profile';
+const token = "eyJhbGciOiJIUzI1NiJ9.eyJST0xFUyI6IlJPTEVfVVNFUiIsInN1YiI6InVzZXJycnIiLCJpYXQiOjE2NTc5NzYyMzMsImV4cCI6MTY1ODAxMjIzM30.c-QGz1iA2BvG6EYwgaEpTRmlz1fQ3LjMdJYDpMuwWC4";
 
 type Props = {}
 
 export default function Profile({}: Props) {
+  const [username, setUsername] = useState('');
+
+  useEffect(()=> {
+    const getProfile = async() => {
+    const response = await axios.get(MY_PROFILE_API,
+    {
+      headers: {
+        'Content-Type': 'application/json',
+        // 'Authorization': 'Bearer '+ token,
+        Authorization: 'Bearer ' + localStorage.getItem('token'),
+      }
+    }
+    );
+    setUsername(response.data.userName);
+  }
+  getProfile();
+  });
+
+
   return (
-    <div>Profile</div>
-  )
+      <div>{username}</div>
+    )
 }
