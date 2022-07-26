d6777e608b815e0a58337d018f627749952bc1fb
piedpipr <43669876+piedpipr@users.noreply.github.com>
2022-07-27 00:14:55 +0600

    public profile template 80% done

--- frontend/simplyblog/src/components/public_profile.tsx
+++ frontend/simplyblog/src/components/public_profile.tsx
@@ -4,48 +4,106 @@ import axios from '../api/axios';
 
 import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
+import { Container } from 'react-bootstrap';
 
 const PUBLIC_PROFILE_API = '/user/';
+const PUBLIC_BLOGS_API = '/blogs/';
 const usernameDummy = 'userrrr';
 
 type Props = {
 }
 
 export default function PublicProfile({}: Props) {
-  const [userdetails, setUserdetails] = useState('');
+  const [userdata, setUserdata] = useState({
+    id:'',
+    userName:'',
+    email:'',
+    bio:'',
+    roles:'',
+  });
+  const [blogsdata, setBlogsdata] = useState(
+    [
+      {
+      id:'',
+      title:'',
+      body:'',
+      publishedDate:null,
+      published:true
+    }
+    ]);
+
   let userName = useParams().userName;
 
   useEffect(()=> {
     const getProfile = async() => {
     const response = await axios.get(PUBLIC_PROFILE_API + userName);
-    setUserdetails(JSON.stringify(response.data));
+    setUserdata(response.data);
     console.log(response.data);
   }
   getProfile();
-  });
+  },[]);
+
+  useEffect(()=> {
+    const getBlogs = async() => {
+    const response = await axios.get(PUBLIC_BLOGS_API + userName);
+    // setBlogsdata(response.data);
+    setBlogsdata(response.data);
+    console.log(response.data);
+  }
+  getBlogs();
+  },[]);
+
 
 
   return (
-      <div>{userdetails}
+    <Container>
+      <div className="d-flex flex-lg-row flex-md-row flex-column p-4">
+      <div>
       <Card style={{ width: '18rem' }}>
       <Card.Img variant="top" src={`https://avatars.dicebear.com/api/avataaars/${userName}.svg`}/>
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
+        <Card.Title>{userdata.userName}</Card.Title>
         <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
+          {userdata.bio}
         </Card.Text>
       </Card.Body>
       <ListGroup className="list-group-flush">
-        <ListGroup.Item>Cras justo odio</ListGroup.Item>
-        <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-        <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+        <ListGroup.Item>Email</ListGroup.Item>
+        <ListGroup.Item>Occupation</ListGroup.Item>
+        <ListGroup.Item>Location</ListGroup.Item>
       </ListGroup>
       <Card.Body>
-        <Card.Link href="#">Card Link</Card.Link>
-        <Card.Link href="#">Another Link</Card.Link>
+        <Card.Link href="#">Twitter</Card.Link>
+        <Card.Link href="#">LinkedIn</Card.Link>
       </Card.Body>
     </Card>
+    </div>
+    <div className="container d-flex flex-wrap">
+      <div className=" align-self-center align-items-center align-content-center">
+        <h1 className="text-center pb-2">Recent Posts</h1>
       </div>
+    {blogsdata.map((blog) => 
+      <div>
+        <Card
+          bg='light'
+          key={blog.id}
+          text='dark'
+          style={{ width: '50vw'}}
+          className="mb-2"
+        >
+          <Card.Body>
+            <Card.Title>{blog.title}</Card.Title>
+            <Card.Text>
+              {blog.body}
+              <br></br>
+              {blog.publishedDate}
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </div>
+)}
+    </div>
+    </div>
+    </Container>
     )
 }
