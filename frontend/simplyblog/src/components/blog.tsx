4aebeb51172a5abeccacb64492bf46b148873b6d
piedpipr <43669876+piedpipr@users.noreply.github.com>
2022-07-28 04:23:00 +0600

    Blog page ready with like unlike and datas

--- frontend/simplyblog/src/components/blog.tsx
+++ frontend/simplyblog/src/components/blog.tsx
@@ -1,13 +1,24 @@
-import {useEffect, useState} from 'react'
+import {useEffect, useState } from 'react'
 import {useParams} from 'react-router-dom'
+import { Card } from 'react-bootstrap';
 import axios from '../api/axios';
+import { Link } from 'react-router-dom';
+import useAuth from '../hooks/useAuth';
+import { LikeStatusType, BlogType }  from '../types/api-types';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faThumbsUp, faThumbsDown } from '@fortawesome/free-solid-svg-icons';
 
 const PUBLIC_SINGLE_BLOG_API = '/blogs/show/';
+const BLOG_LIKES_API = '/blogs/likes/';
+const BLOG_UNLIKES_API = '/blogs/unlikes/';
 
 export default function Blog(){
-    const [blogdata, setBlogdata] = useState({});
-    
+    const [blogdata, setBlogdata] = useState<BlogType>();
+    const [likes, setLikes] = useState(0);
+    const [unlikes, setUnlikes] = useState(0);
+    const [likeStatus, setLikeStatus] = useState<LikeStatusType>();
     let blogId = useParams().id;
+    let userAuth = useAuth();
 
     useEffect(()=> {
         const getBlog = async() => {
@@ -16,9 +27,64 @@ export default function Blog(){
             console.log(response.data);
         }
         getBlog();
-    }, []);
+    }, [blogId]);
+    useEffect(()=> {
+      const getBlogLikes = async() => {
+          const response = await axios.get(BLOG_LIKES_API+blogId);
+          setLikes(response.data);
+          console.log(response.data);
+      }
+      getBlogLikes();
+  }, [blogId]);
+    useEffect(()=> {
+      const getBlogUnLikes = async() => {
+          const response = await axios.get(BLOG_UNLIKES_API+blogId);
+          setUnlikes(response.data);
+          console.log(response.data);
+      }
+      getBlogUnLikes();
+  }, [blogId]);
+  useEffect(()=> {
+    if(userAuth.isAuthenticated){
+    const likeStatus = async() => {
+        const response = await axios.get('blogs/'+blogId+'/'+userAuth.username);
+        console.log(response.data);
+        setLikeStatus(response.data);
+    }
+    likeStatus();
+}}, [blogId, userAuth.isAuthenticated]);
   return (
-    <div>{JSON.stringify(blogdata)}</div>
+    <div>
+      <div className="p-5 mb-4 bg-light rounded-3">
+      <div className="container-fluid py-5">
+        <Card.Img 
+          variant="top"
+          src="https://random.imagecdn.app/900/300"/>
+        <h1 className="display-5 fw-bold mt-5 text-center">{blogdata?.title}</h1>
+        <Link to={`/user/${blogdata?.myUsers.userName}`} className="text-decoration-none">
+        <p className="fs-10 mt-2 text-center">
+        <img 
+        src={`https://avatars.dicebear.com/api/avataaars/${blogdata?.myUsers.userName}.svg`}
+        style={{height: '30px', width: '30px', borderRadius: '50%'}}
+        />{blogdata?.myUsers.userName}</p>
+        </Link>
+        <p className="col-md-8 fs-4 mt-5">{blogdata?.body}</p>
+        <button
+          className="btn btn-primary btn-sm me-2"
+          disabled={(likeStatus?.likedBy?.userName===userAuth.username)?true:false} 
+          type="button"
+          >{likes} Like <FontAwesomeIcon icon={faThumbsUp} />
+        </button>
+        <button
+          className="btn btn-secondary btn-sm"
+          disabled={(likeStatus?.unlikedBy?.userName===userAuth.username)?true:false}
+          type="button">
+          {unlikes} Unlike <FontAwesomeIcon icon={faThumbsDown} />
+        </button>
+        <p className="fs-10 text-end">Published on: {blogdata?.publishedDate}</p>
+      </div>
+      </div>  
+    </div>
   )
 }
 
