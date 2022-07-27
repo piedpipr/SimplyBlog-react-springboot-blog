c481f8f40df0583208c89838eb0b8d19d44232f7
piedpipr <43669876+piedpipr@users.noreply.github.com>
2022-07-28 04:21:25 +0600

    Added modular type file for APIs

---
+++ frontend/simplyblog/src/types/api-types.tsx
@@ -0,0 +1,64 @@
+export interface myUsersType {
+    id: number,
+    userName: string,
+    password: string,
+    email: string,
+    bio: string,
+    roles: string,
+    active: boolean,
+  }
+
+export interface BlogType {
+    id: number,
+    title: string,
+    body: string,
+    publishedDate: string,
+    published: boolean,
+    myUsers: {
+      id: number,
+      userName: string,
+      password: string,
+      email: string,
+      bio: string,
+      roles: string,
+      active: boolean,
+  }
+}
+
+export interface LikeStatusType {
+        id: number,
+        blog: {
+          id: number,
+          title: string,
+          body: string,
+          publishedDate: string,
+          published: boolean,
+          myUsers: {
+            id: number,
+            userName: string,
+            password: string,
+            email: string,
+            bio: string,
+            roles: string,
+            active: boolean,
+        },
+        },
+        likedBy: {
+            id: number,
+            userName: string,
+            password: string,
+            email: string,
+            bio: string,
+            roles: string,
+            active: boolean,
+        },
+        unlikedBy: {
+            id: number,
+            userName: string,
+            password: string,
+            email: string,
+            bio: string,
+            roles: string,
+            active: boolean,
+        }
+}
